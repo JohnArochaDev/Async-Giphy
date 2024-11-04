a0d1c8e12c1d7c88f1c6d00e743e7db46bb57f51
John Arocha <john.arocha@clover.com>
2024-11-04 10:47:33 -0700

    wtf

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
 require('dotenv').config();
 
-// Print out value of API key stored in .env file
-console.log(process.env.API_KEY)
+let gifHolder = document.querySelector('.gif')
+
+let data;
+
+async function getImage(query) {
+    const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=Iws8aBxubBauhh369vrIDK9NmqleBunf&q=${query}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`;
+
+    try {
+        const response = await fetch(endpoint);
+        const jsonData = await response.json();
+        data = jsonData.data;
+        for (let i = 0; i < data.length; i++) {
+            const img = document.createElement('img');
+            img.src = data[i].images.fixed_height.url;
+            gifHolder.appendChild(img);
+        }
+    } catch (error) {
+        console.error("You've had the following error: \n", error);
+    }
+}
+
+getImage('tacobell');
