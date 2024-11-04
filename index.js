b1314e5362f08a799d7ddad2e21108989f3c4fe5
John Arocha <john.arocha@clover.com>
2024-11-04 13:29:13 -0700

    added info

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-require('dotenv').config();
+import key from './test'
 
-// let gifHolder = document.querySelector('.gif')
+console.log(key)
 
-// key = env.
+let gifHolder = document.querySelector('.gif')
 
 let data;
 
 async function getImage(query) {
-    const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=Iws8aBxubBauhh369vrIDK9NmqleBunf&q=${query}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`;
-
+    const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${key}&q=${query}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`;
+    
     try {
         const response = await fetch(endpoint);
         const jsonData = await response.json();
         data = jsonData.data;
         for (let i = 0; i < data.length; i++) {
-            // const img = document.createElement('img');
-            // img.src = data[i].images.fixed_height.url;
-            // gifHolder.appendChild(img);
+            const img = document.createElement('img');
+            img.src = data[i].images.fixed_height.url;
+            gifHolder.appendChild(img);
             console.log(data[i].embed_url)
         }
     } catch (error) {
---
+++ test.js
@@ -0,0 +1,5 @@
+require('dotenv').config();
+
+let key = process.env.API_KEY
+
+module.exports = key
